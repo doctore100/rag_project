dd1a79e87c83d69b3061b2dd85255de71fe9812a
david <dg071018@gmail.com>
2025-08-03 14:27:11 +0200

    Add methods to create vector database and enable `pgvector` extension in `DatabaseManager`.

--- infrastructure/database/__init__.py
+++ infrastructure/database/__init__.py
@@ -1,9 +1,12 @@
+from openai import vector_stores
+
 from config import create_settings as s
 from langchain_postgres.vectorstores import PGVector
-
+import psycopg
+from psycopg import OperationalError, Error as PsycopgError
 
 class DatabaseManager:
-    def __init__(self,model_embeddings,collection_name: str = None, ):
+    def __init__(self,model_embeddings=None,collection_name: str = None, ):
         self.settings = s()
         self.collection_name = collection_name
         self.embeddings = model_embeddings
@@ -20,9 +23,69 @@ class DatabaseManager:
         if self.settings.environment == "development":
             return f"postgresql+psycopg://{self.settings.database.db_user}:{self.settings.database.db_password}@{self.settings.database.db_host}:{self.settings.ssh_connection.db_port}/{self.settings.database.collection_name}"
         return f"sqlite:///test_{self.settings.database.collection_name}.db"
-    def __create_vector_db_if_not_exist(self):
-        pass
+
+    def create_vector_db_if_not_exist(self):
+
+        try:
+            with psycopg.connect(
+                    host=self.settings.database.db_host,
+                    port=self.settings.ssh_connection.local_bind_port,
+                    dbname=self.settings.database.db_name,
+                    user=self.settings.database.db_user,
+                    password=self.settings.database.db_password.get_secret_value(),
+                    autocommit=True  # debe estar en True para CREATE DATABASE
+            ) as conn:
+                with conn.cursor() as cur:
+                    from psycopg import sql
+                    vector_stores_database = self.settings.database.collection_bb_name
+
+                    cur.execute(
+                        "SELECT 1 FROM pg_database WHERE datname = %s",
+                        (vector_stores_database,)
+                    )
+
+                    exists = cur.fetchone()
+                    if not exists:
+                        # Create the database if it doesn't exist
+                        cur.execute(
+                            sql.SQL("CREATE DATABASE {}").format(sql.Identifier(vector_stores_database))
+                        )
+                        print(f"✔️ Base de datos '{vector_stores_database}' creada.")
+                    else:
+                        print(f"ℹ️ La base de datos '{vector_stores_database}' ya existe.")
+
+        except OperationalError as oe:
+            print("❌ No se pudo conectar para crear la base de datos:", oe)
+            raise
+        except PsycopgError as pe:
+            print("❌ Error SQL creando la base de datos:", pe)
+            raise
+
+    def enable_pgvector_extension(self):
+        try:
+            with psycopg.connect(
+                    host=self.settings.database.db_host,
+                    port=self.settings.ssh_connection.local_bind_port,
+                    dbname=self.settings.database.collection_bb_name,
+                    user=self.settings.database.db_user,
+                    password=self.settings.database.db_password.get_secret_value(),
+                    autocommit=True  # debe estar en True para CREATE DATABASE
+                    # recomendamos autocommit, aunque CREATE EXTENSION sí permite estar en transacción
+            ) as conn:
+                with conn.cursor() as cur:
+                    cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+                    print(f"🧠 Extensión 'vector' habilitada en la base de datos: {self.settings.database.collection_bb_name}")
+        except OperationalError as oe:
+            print("❌ No se pudo conectar para habilitar extensión:", oe)
+            raise
+        except PsycopgError as pe:
+            print("❌ Error SQL habilitando extensión:", pe)
+            raise
+
+
+
     def initialize_vector_store(self, pre_delete_collection: bool = True):
+        coll_name = self.collection_name if self.collection_name else self.settings.database.collection_name
         """
         Initializes the vector store using the provided configuration and connection
         parameters. The method sets up a PGVector instance for storing embeddings along
@@ -43,12 +106,12 @@ class DatabaseManager:
         try:
             self.vector_store = PGVector(
                 embeddings=self.embeddings,
-                collection_name=self.collection_name if self.collection_name else self.settings.database.collection_name,
-                connection=self.settings.ssh_connection_string,
+                collection_name=coll_name,
+                connection=f"postgresql+psycopg://{self.settings.database.db_user}:{self.settings.database.db_password.get_secret_value()}@{self.settings.database.db_host}:{self.settings.ssh_connection.local_bind_port}/{self.settings.database.collection_bb_name}",
                 use_jsonb=pre_delete_collection,  # Usar JSONB para metadatos
                 pre_delete_collection=pre_delete_collection
             )
-            print(f"✓ Vector store inicializado: colección '{self.collection_name}'")
+            print(f"✓ Vector store inicializado: colección {coll_name}")
             return self.vector_store
         except Exception as e:
             print(f"✗ Error inicializando vector store: {e}")
